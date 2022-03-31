3a4c14a3e555addda0de743c6d6eea82b4360fa8
samkb@420 <45421062+samkb420@users.noreply.github.com>
2022-03-31 05:01:58 +0300

    Create demo.sh

---
+++ demo.sh
@@ -0,0 +1,10 @@
+sudo docker run --detach \
+  --hostname git-uat.oloosirikonschool.sc.ke/ \
+  --publish 443:443 --publish 80:80 --publish 22:22 \
+  --name gitlab \
+  --restart always \
+  --volume $GITLAB_HOME/config:/etc/gitlab \
+  --volume $GITLAB_HOME/logs:/var/log/gitlab \
+  --volume $GITLAB_HOME/data:/var/opt/gitlab \
+  --shm-size 256m \
+  gitlab/gitlab-ee:latest
