e9308da7c0b7f79b0587e78a9a0ab046cb9119a2
samkb@420 <45421062+samkb420@users.noreply.github.com>
2022-03-31 05:04:44 +0300

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,7 +1,7 @@
 sudo docker run --detach \
   --hostname git-uat.oloosirikonschool.sc.ke \
   --publish 443:443 --publish 80:80 --publish 23:23 \
-  --name gitlab \
+  --name gitlab-uat \
   --restart always \
   --volume $GITLAB_HOME/config:/etc/gitlab \
   --volume $GITLAB_HOME/logs:/var/log/gitlab \
